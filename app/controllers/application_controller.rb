89254258c0b92185aaccac34c4c1ae74b3aa38d2
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-04 19:47:57 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,6 +25,11 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
   end
 
 
